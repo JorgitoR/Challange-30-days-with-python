7b3981fb530e3a6f1b1ca0d1b42475d5ee138a51
unknown <jorgitouribe@gmail.com>
2021-09-09 09:19:05 -0500

    Python Day 14

---
+++ Day 14/readme.md
@@ -0,0 +1,4 @@
+#Paso 1
+Install python -m pip install pipenv
+pipenv shell --python 3.9
+pipenv install flask gunicorn uvicorn fastapi
---
+++ Day 14/server.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello_world():
+	return "Hello, world. this is flask"
